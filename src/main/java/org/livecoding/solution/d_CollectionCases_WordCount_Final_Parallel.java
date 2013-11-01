7c7d7164f67bde2bd90bc68ece1a5cbdfc7776a2
upeter <peter.urs@gmail.com>
2013-11-01 11:56:00 +0100

    adjustments

---
+++ src/main/java/org/livecoding/solution/d_CollectionCases_WordCount_Final_Parallel.java
@@ -0,0 +1,58 @@
+package org.livecoding.solution;
+
+import java.util.*;
+
+import static org.junit.Assert.assertEquals;
+
+public class d_CollectionCases_WordCount_Final_Parallel {
+
+
+    public static void main(String[] args) {
+        //countLongestWordFunctional();
+        countLongestWordParallelFunctional();
+
+    }
+
+
+    private static void countLongestWordFunctional() {
+        List<String> lines = Arrays.asList("JFall rocks!", "Java8 is almost there");
+
+        int lengthLongestWord = lines.parallelStream()
+                .map(line -> Arrays.asList(line.split(" ")).stream()
+                        .map(String::length)
+                        .reduce(Math::max).orElse(0))
+                .reduce(Math::max).orElse(0);
+
+
+        lengthLongestWord = lines.stream()
+                .mapToInt(line -> Arrays.asList(line.split(" ")).stream()
+                        .mapToInt(String::length)
+                        .max().orElse(0))
+                .max().orElse(0);
+
+        assertEquals("almost".length(), lengthLongestWord);
+    }
+
+
+    private static void countLongestWordParallelFunctional() {
+        List<String> lines = getManyLines();
+        long current = System.currentTimeMillis();
+
+        int lengthLongestWord = lines.parallelStream()
+                .map(line -> Arrays.asList(line.split(" ")).stream()
+                        .map(String::length)
+                        .reduce(Math::max).orElse(0))
+                .reduce(Math::max).orElse(0);
+
+        long elapsed = System.currentTimeMillis() - current;
+        System.out.println("Elapsed " + elapsed);
+    }
+
+    public static List<String> getManyLines() {
+        List<String> lines  = new ArrayList<>();
+        for (int i = 0; i < 20000000; i++) {
+            lines.add("JFall rocks " + new Integer(i).toString() + " times!");
+        }
+        return lines;
+    }
+}
