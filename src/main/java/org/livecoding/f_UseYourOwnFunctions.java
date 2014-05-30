31c979d7bb0261976c90c5ea4c7aef4af985d047
upeter <peter.urs@gmail.com>
2014-05-30 16:26:33 +0200

    keep imports

--- src/main/java/org/livecoding/f_UseYourOwnFunctions.java
+++ src/main/java/org/livecoding/f_UseYourOwnFunctions.java
@@ -15,33 +15,45 @@ public class f_UseYourOwnFunctions {
     public static final String JAVA_8_REGEXP = ".*[Jj][Aa][Vv][Aa][\\s]?8.*";
 
     public static void main(String[] args) {
-        String url = StreamingTweetFilter.class.getResource("/many_tweets.json").toExternalForm();
+        String url = StreamingTweetFilter.class.getResource("/tweets.json").toExternalForm();
 
     }
 
     abstract static class StreamingTweetFilter {
 
-        public List<Tweet> filterTweets(String url, Predicate<Tweet> tweetFilter) {
-            try (BufferedReader br = new BufferedReader(new InputStreamReader(new URL(url).openStream()))) {
-                return br.lines()
-                        .parallel()
-                        .map(Tweet::new)
-                        .filter(tweetFilter)
-                        .collect(Collectors.toList());
+        public List<Tweet> filterTweets(String searchQuery) {
+            List<Tweet> collected = new ArrayList<>();
+            try (BufferedReader br = new BufferedReader(new InputStreamReader(new URL(searchQuery).openStream()))) {
+                String json = "";
+                while ((json = br.readLine()) != null) {
+                    Tweet tweet = new Tweet(json);
+                    //filter tweets
+                    if (doFilterTweet(tweet)) {
+                        collected.add(tweet);
+                    }
+                }
             } catch (IOException e) {
                 throw new RuntimeException(e);
             }
+            return collected;
         }
+
+        abstract boolean doFilterTweet(Tweet tweet);
+
     }
 
 
-    static class Java8TweetFilter extends StreamingTweetFilter{
+    static class Java8TweetFilter extends StreamingTweetFilter {
 
         protected boolean doFilterTweet(Tweet tweet) {
             return tweet.getMessage().matches(JAVA_8_REGEXP);
 
         }
     }
+
+
+
+
 
 
 
